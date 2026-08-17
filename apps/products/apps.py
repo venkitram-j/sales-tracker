ecbbdb9b88c2027d9808a022d522fabf26c08859
Venkitram J <venkitramj@gmail.com>
2026-08-17 20:54:57 +0530

    barebones project

--- apps/products/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.products"
-    label = "products"
-    verbose_name = "Products"
